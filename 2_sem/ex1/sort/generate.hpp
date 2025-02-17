4547f227806cb35c9c852f21e757b9ff4c71e96b
Сергей Гурьянов <gurjanov.sergey@icloud.com>
2025-02-17 23:07:44 +0300

    sort до СMake и тестов

---
+++ 2_sem/ex1/sort/generate.hpp
@@ -0,0 +1,12 @@
+#ifndef GEN_HPP
+#define GEN_HPP
+
+#include <random>
+
+namespace gsm {
+    namespace generate {
+	    void generate_array(int *arr, const int n, const int max_value);
+    }
+}
+
+#endif
---
+++ 2_sem/ex1/sort/io.hpp
@@ -0,0 +1,14 @@
+#ifndef IO_HPP
+#define IO_HPP
+
+namespace gsm {
+	namespace io {
+		int input_user_max_value ();
+		int input_user_size ();
+		void print_array (const char* const comment, const int * const arr, const int n);
+	}
+	
+}
+
+#endif
+
